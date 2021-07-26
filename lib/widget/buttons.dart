26182d97d29a18d3db94f97082c4fbb4e0e465e3
shiby francis <shibykfrancis@gmail.com>
2021-07-26 16:34:28 +0530

    push with students list

--- lib/widget/buttons.dart
+++ lib/widget/buttons.dart
@@ -5,12 +5,12 @@ import 'package:nurture/widget/actions.dart';
 import 'package:nurture/widget/spinner.dart';
 import 'package:nurture/model/login_model.dart';
 
-List<Widget> loginButtons(formKey, loginRequestModel) {
+List<Widget> loginButtons(formKey, loginRequestModel, {bool asim = false}) {
   return <Widget>[
     GestureDetector(
       child: Container(
           height: 40,
-          width: 300, //double.infinity,
+          width: double.infinity,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(6),
             gradient: LinearGradient(
@@ -41,4 +41,90 @@ List<Widget> loginButtons(formKey, loginRequestModel) {
       },
     ),
   ];
+}
+
+List<Widget> otpButtons(formKey, loginRequestModel) {
+  return <Widget>[
+    GestureDetector(
+      child: Container(
+          height: 40,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            gradient: LinearGradient(
+              colors: [
+                Color(0xff43CEA2),
+                Color(0xff279DD4),
+              ],
+            ),
+          ),
+          child: Center(
+            child: Text(
+              "Submit",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          )),
+      onTap: () async {
+        LoginResponseModel data =
+            await validateAndLogin(formKey, loginRequestModel);
+        if (data.statuscode == "200") {
+          print(data.message);
+          print(data.response.access_token);
+        } else {
+          print(data);
+        }
+      },
+    ),
+  ];
+}
+
+Widget studentsList() {
+  return ListTile(
+      leading: CircleAvatar(
+        radius: 25,
+        backgroundColor: Colors.pink.shade300,
+        // backgroundImage: AssetImage(img),
+      ),
+      title: Text("Asim Muhammad"),
+      isThreeLine: true,
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Student Id 849302hjk",
+            style: TextStyle(fontSize: 11),
+          ),
+          Text("Al Mina aljadeed", style: TextStyle(fontSize: 11))
+        ],
+      ),
+      trailing: IconButton(
+        onPressed: () {},
+        icon: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.green,
+        ),
+      ));
+}
+
+Widget outstandingPayment({bool asim = false}) {
+  return ListTile(
+    leading: Checkbox(
+      value: asim,
+      onChanged: (value) {
+        /* setState(() {
+                this.asim = value!;
+              });*/
+      },
+      side: BorderSide(color: Colors.green),
+      shape: CircleBorder(),
+      activeColor: Colors.green,
+    ),
+    title: Text("Asim Muhammad", style: TextStyle(color: Colors.grey)),
+    selected: true,
+    horizontalTitleGap: 1,
+    trailing: Text("300 KD", style: TextStyle(color: Colors.grey)),
+  );
 }
--- lib/widget/inputs.dart
+++ lib/widget/inputs.dart
@@ -26,6 +26,35 @@ List<Widget> loginInputs(loginRequestModel) {
     ),
     SizedBox(
       height: 35,
+    ),
+  ];
+}
+
+List<Widget> otpInputs(loginRequestModel) {
+  return <Widget>[
+    TextFormField(
+      key: Key('pin'),
+      decoration: InputDecoration(
+          labelText: ' Enter PIN',
+          enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey[300])),
+          labelStyle: TextStyle(color: Colors.grey[400])),
+      validator: EmailFieldValidator.validate,
+      onSaved: (String value) => loginRequestModel.email = value,
+    ),
+    TextFormField(
+      key: Key('confirm'),
+      decoration: InputDecoration(
+          labelText: 'Confirm PIN',
+          enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey[300])),
+          labelStyle: TextStyle(color: Colors.grey[400])),
+      obscureText: true,
+      validator: PasswordFieldValidator.validate,
+      onSaved: (String value) => loginRequestModel.password = value,
+    ),
+    SizedBox(
+      height: 35,
     )
   ];
 }

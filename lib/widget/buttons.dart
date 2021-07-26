b989181b81f5d81ee0fe2fcc4361b2aa94e1680d
shiby francis <shibykfrancis@gmail.com>
2021-07-26 17:58:45 +0530

    push new widget

--- lib/widget/buttons.dart
+++ lib/widget/buttons.dart
@@ -5,7 +5,7 @@ import 'package:nurture/widget/actions.dart';
 import 'package:nurture/widget/spinner.dart';
 import 'package:nurture/model/login_model.dart';
 
-List<Widget> loginButtons(formKey, loginRequestModel, {bool asim = false}) {
+List<Widget> loginButtons(formKey, loginRequestModel, ) {
   return <Widget>[
     GestureDetector(
       child: Container(
@@ -126,5 +126,214 @@ Widget outstandingPayment({bool asim = false}) {
     selected: true,
     horizontalTitleGap: 1,
     trailing: Text("300 KD", style: TextStyle(color: Colors.grey)),
+  );
+}
+
+Widget installment() {
+  return Padding(
+    padding: const EdgeInsets.all(15),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Installment 1"),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Tution",
+              style: TextStyle(color: Colors.grey),
+            ),
+            Text(
+              "0KD",
+              style: TextStyle(color: Colors.grey),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Transportation",
+              style: TextStyle(color: Colors.grey),
+            ),
+            Text(
+              "0KD",
+              style: TextStyle(color: Colors.grey),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Other",
+              style: TextStyle(color: Colors.grey),
+            ),
+            Text(
+              "0KD",
+              style: TextStyle(color: Colors.grey),
+            )
+          ],
+        )
+      ],
+    ),
+  );
+}
+
+Widget receiptBox(    {bool failed = true}) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+    child: Container(
+      height: 103,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: .5),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                  height: 99,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          // crossAxisAlignment: CrossAxisAlignment.,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Transaction Id",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 12),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "DateTime",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 12),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Payment Id",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 12),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Amount",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 12),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "txt1a",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "txt1b",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "txt1c",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "300 KD",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: failed
+                                        ? Colors.greenAccent
+                                        : Colors.red,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )),
+              SizedBox(
+                height: 83,
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    failed
+                        ? Icon(
+                            Icons.check_circle_outline,
+                            color: Colors.greenAccent,
+                          )
+                        : Icon(
+                            Icons.close_rounded, //lose_outlined,
+                            color: Colors.red,
+                          ),
+                    Container(
+                        height: 20,
+                        width: 30,
+                        decoration: BoxDecoration(
+                            color: Colors.green[50],
+                            borderRadius: BorderRadius.circular(10)),
+                        child: failed
+                            ? Center(
+                                child: Text(
+                                "Download Receipt",
+                                style: TextStyle(
+                                    fontSize: 8.5, color: Colors.green),
+                              ))
+                            : Center(
+                                child: Text(
+                                  "failed transaction",
+                                  style: TextStyle(
+                                      fontSize: 8.5, color: Colors.grey),
+                                ),
+                              ))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    ),
   );
 }
